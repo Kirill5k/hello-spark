d3a1ee3049a6fc31986bfba5e4cdd672dc8bffcf
Kirill <immotional@aol.com>
2019-10-05 12:22:16 +0100

    dataframes basic operations

--- src/main/scala/dataframes/Operations2.scala
+++ src/main/scala/dataframes/Operations2.scala
@@ -19,17 +19,22 @@ object Operations2 extends App {
     .option("inferSchema", "true")
     .csv("data/Netflix_2011_2016.csv")
 
+  println("columns")
   println(df.columns.toList)
+  println("scheme")
   df.printSchema()
+  println("first 5 columns")
   df.show(5)
-  df.describe()
+  println("dataframe description")
+  df.describe().show()
 
   println("high/volume ratio")
-  val dfWithHVRation = df.withColumn("HV Ratio", df("High") / df("Volume"))
-  dfWithHVRation.show(5)
+  val dfWithHVRatio = df.withColumn("HV Ratio", df("High") / df("Volume"))
+  dfWithHVRatio.show(5)
 
   println("peak high")
-  df.select(max("High")).show()
+//  df.select(max("High")).show()
+  df.orderBy($"High".desc).show(1)
 
   println("close mean")
   df.select(mean("Close")).show()
@@ -42,9 +47,9 @@ object Operations2 extends App {
   println(df.filter($"Close" < 600).count())
 
   println("% of time when High was greater than $500")
-  val highGreater500 = df.filter($"High" > 600).count()
+  val highGreater500 = df.filter($"High" > 500).count()
   val total = df.count()
-  println(s"total = $total, highGreaterThan500 = $highGreater500, % = ${highGreater500 * 100 / total}")
+  println(s"total = $total, highGreaterThan500 = $highGreater500, % = ${highGreater500 * 100.00 / total}")
   println()
 
   println("pearson correlation between High and Volume")
@@ -56,7 +61,8 @@ object Operations2 extends App {
     .groupBy("Year")
     .max()
     .select($"Year", $"max(High)")
-    .show(4)
+    .orderBy("Year")
+    .show(10)
 
   println("average close per month")
   val dfWithMonth = df.withColumn("Month", month(df("Date")))
