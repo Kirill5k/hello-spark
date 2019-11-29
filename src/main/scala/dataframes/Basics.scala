b3d8768ea9a4e9175a54bd0eafdf032245dfdaa5
kirill5k <immotional@aol.com>
2019-11-29 19:42:48 +0000

    added schema declaration example

--- src/main/scala/dataframes/Basics.scala
+++ src/main/scala/dataframes/Basics.scala
@@ -2,6 +2,7 @@ package dataframes
 
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.types.{DoubleType, LongType, StringType, StructField, StructType}
 
 object Basics extends App {
   System.setProperty("hadoop.home.dir", "C:/winutils/")
@@ -13,7 +14,20 @@ object Basics extends App {
     .config("spark.master", "local[*]")
     .getOrCreate()
 
+  val carsSchema = StructType(Array(
+    StructField("Name", StringType, nullable = false),
+    StructField("Miles_per_Gallon", DoubleType),
+    StructField("Cylinders", LongType),
+    StructField("Displacement", DoubleType),
+    StructField("Horsepower", LongType),
+    StructField("Weight_in_lbs", LongType),
+    StructField("Acceleration", DoubleType),
+    StructField("Year", StringType),
+    StructField("Origin", StringType)
+  ))
+
   val firstDf = session.read
+    .schema(carsSchema)
     .format("json")
     .option("inferSchema", "true")
     .load("data/cars.json")
